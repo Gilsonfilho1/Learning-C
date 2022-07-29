cc0719cde52a19ec613d473b96f3c000998c03e9
Gilson filho <101582105+Gilson-21@users.noreply.github.com>
2022-07-29 20:40:20 -0300

    Add files via upload

---
+++ Lista 01/Questão_01/main.c
@@ -0,0 +1,17 @@
+// Faça um programa que leia um número inteiro e o imprima.
+
+
+#include <stdio.h>
+
+int main(){
+
+	int numero1;
+
+    printf("Digite um numero:");
+	scanf("%d", &numero1);
+
+	printf("\nNumero lido: %d",numero1);
+
+return 0;
+
+}
